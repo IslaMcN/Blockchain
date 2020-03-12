51e581db8035aae036ace493498c8c5010b32ea8
Isla <fallenacademy@gmail.com>
2020-03-12 14:01:53 -0700

    Creating a login and not too sure if I will get it implemented but it would be nice to have some sort of auth for my lambda coins

---
+++ basic_wallet_p/login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import axiosWithAuth from './utils/axiosWithAuth';
+
+class Login extends React.Component{
+    state = {
+        credentials: {
+            username: '',
+            password: ''
+        }
+    };
+    handleChange = e => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
+    handleSubmit = e 
+}
--- basic_wallet_p/transactions.js
+++ basic_wallet_p/transactions.js
@@ -18,6 +18,65 @@ const TransactionList = ({transactions, updateTransactions}) => {
     const saveEdit = e => {
         e.preventDefault();
         axios
-        .put('http://localhost:5000/')
-    }
+        .put('http://localhost:5000/NOTTOOSUREYET/:id')
+        .then(res => {
+            console.log('This is coming from TransactionList', res)
+            updateTransactions(res.data)
+        })
+        .catch(err => {
+            console.log('Nope', err)
+        });
+
+    };
+    const deleteTransaction = bit => {
+        axios
+        .delete('http://localhost:50000/NOTTOOSUREYET/:id')
+        .then(res => {
+            console.log('This is coming from delete in TransactionList', res)
+            updateTransactions(res.data)
+        })
+        .catch(err => {
+            console.log(err)
+        });
+    };
+
+    return(
+        <div>
+            <p>transactions</p>
+            <ul>
+                {transactions.map(bit => {
+                    <li onClick={() => editTransaction(bit)}>
+                        <span>
+                            <span onClick={()=> deleteTransaction(bit)}> X </span>
+                        </span>
+                    </li>
+                })}
+            </ul>
+            {edit && (
+                <form onSubmit={saveEdit}>
+                    <legend>edit transactions</legend>
+                    <label>
+
+                        Sender:
+                        <input onChange = {e => setTransactionToEdit({ ...transactionToEdit, sender: e.target.value})}
+                        value = {transactionToEdit.sender}/>
+                    </label>
+                    <label>
+                        Recipient:
+                        <input onChange = {e => setTransactionToEdit({...transactionToEdit, recipient: e.target.value})} value = {transactionToEdit.recipient}/>
+                    </label>
+                    <label>
+                        Amount:
+                        <input onChange = {e => setTransactionToEdit({...setTransactionToEdit, amount: e.target.value})} value = {transactionToEdit.amount}/>
+                    </label>
+                    <div>
+                        <button type="submit">Save</button>
+                        <button onClick={() => setEdit(false)}>Cancel</button>
+                    </div>
+                </form>
+            )}
+        </div>
+    )
 }
+
+export default TransactionList;
